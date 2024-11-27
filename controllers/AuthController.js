d683b4c000a8f32b6920699f140b65f18dcfb54a
Silvio Watakabe <swat2014@outlook.com>
2024-11-26 19:52:01 -0500

    - Modelagem do form register - Inclusão de método save em auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render('auth/login')
@@ -5,5 +7,18 @@ module.exports = class AuthController {
 
     static register(req, res) {
         res.render('auth/register')
+    }
+
+    static save(req, res) {
+        const { name, email, password, confirmpassword } = req.body;
+        const data = { name, email, password, confirmpassword };
+        let msg = 'Cadastrado com sucesso!'
+        // Caso a senha não seja a mesma da confirmação, redireciona o usuário para o form
+        if (password !== confirmpassword) {
+            msg = 'A senha não confere com a conformação de senha!'
+            res.redirect('/')
+        }
+        User.create(data)
+        res.redirect('/dashboard')
     }
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const AuthController = require('../controllers/AuthController')
 
 router.get('/login', AuthController.login)
 router.get('/register', AuthController.register)
+router.post('/register', AuthController.save)
 
 module.exports = router
